5f59852ad3a6b8e8a832be4b2b176ee1a5b9e687
gokseninyuksel <Goksenin.yuksel@student.ru.nl>
2023-01-04 18:40:45 +0100

    Added the arrival module for api

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,28 @@
 #[warn(non_snake_case)]
-pub const API: &str = "https://opensky-network.org/api/flights/departure?";
 pub mod departures;
+pub mod arrivals;
 pub mod data_builder;
+use serde::{Serialize,Deserialize};
+
+
+pub const API_DEPARTURE: &str = "https://opensky-network.org/api/flights/departure?";
+pub const API_ARRIVAL: &str = "https://opensky-network.org/api/flights/arrival?";
+type AirportResponses = Vec<AirportResponse>;
+
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct AirportResponse {
+    icao24: String,
+    first_seen: Option<usize>,
+    est_departure_airport: Option<String>,
+    last_seen: Option<usize>,
+    est_arrival_airport: Option<String>,
+    callsign: Option<String>,
+    est_departure_airport_horiz_distance: Option<usize>,
+    est_departure_airport_vert_distance:  Option<usize>,
+    est_arrival_airport_horiz_distance:  Option<usize>,
+    est_arrival_airport_vert_distance:  Option<usize>,
+    departure_airport_candidates_count:  Option<usize>,
+    arrival_airport_candidates_count:  Option<usize>,
+}
